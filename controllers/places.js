b5b8ca7556597626d16c11f81f548fa270a339c1
FRANZ RHANICO <109740904+rhanico@users.noreply.github.com>
2023-04-30 05:14:20 -0700

    .

--- controllers/places.js
+++ controllers/places.js
@@ -1,28 +1,35 @@
 const router = require('express').Router()
 
 
-router.get('/new', (req, res) => {
-  res.render('places/new')
-})
-
 
 //Get /places
-router.get('/', (req,res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: './images/resto1.jpg'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: './images/resto2.jpg'
-      }];
-      
+router.get('/', (req,res) => {  
+  let places = [{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: './images/resto1.jpg'
+  }, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: './images/resto2.jpg'
+  }];  
     res.render('places/index', { places })
 })
 
+router.post('/', (req, res) => {
+  console.log(req.body)
+  res.send('POST /places')
+})
+
+
+router.get('/new', (req, res) => {
+  res.render('places/new')
+}) 
+
+
 module.exports = router
+
--- index.js
+++ index.js
@@ -10,12 +10,13 @@ app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended:true}))
 
-// Controllers & Routes
-app.use('/places', require('./controllers/places'))
-
 app.get('/', (req, res) => {
     res.render('home')
 })
+
+// Controllers & Routes
+app.use('/places', require('./controllers/places'))
+
 
 app.get('*', (req, res) => {
     res.render('error404')
