342e29d55ef2973a79ab9beacb214e7fa967816c
FRANZ RHANICO <109740904+rhanico@users.noreply.github.com>
2023-04-29 22:35:37 -0700

    Added 404 page

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
 })
 
 app.get('*', (req, res) => {
-    res.status(404).send(' <h1> 404 Page </h1>')
+    res.render(404).send(' <h1> 404 Page </h1>')
 })
 
 app.listen(process.env.PORT)
