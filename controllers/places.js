3d06c4624a161a9e1e6497c76e690ed75560ab4a
FRANZ RHANICO <109740904+rhanico@users.noreply.github.com>
2023-04-30 05:28:15 -0700

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,29 +1,28 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
 
-
-
-//Get /places
-router.get('/', (req,res) => {  
-  let places = [{
-    name: 'H-Thai-ML',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Thai, Pan-Asian',
-    pic: './images/resto1.jpg'
-  }, {
-    name: 'Coding Cat Cafe',
-    city: 'Phoenix',
-    state: 'AZ',
-    cuisines: 'Coffee, Bakery',
-    pic: './images/resto2.jpg'
-  }];  
+router.get('/', (req, res) => {
     res.render('places/index', { places })
 })
 
+
 router.post('/', (req, res) => {
-  console.log(req.body)
-  res.send('POST /places')
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+
+  places.push(req.body)
+  res.redirect('/places')
 })
+
+
 
 
 router.get('/new', (req, res) => {
