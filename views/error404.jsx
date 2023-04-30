24117efe051e3f44a303a0c67a3e3100bd263079
FRANZ RHANICO <109740904+rhanico@users.noreply.github.com>
2023-04-30 01:37:00 -0700

    added img files

--- views/error404.jsx
+++ views/error404.jsx
@@ -7,6 +7,13 @@ function error404 () {
           <main>
               <h1>404: PAGE NOT FOUND</h1>
               <p>Oops, sorry, we can't find this page!</p>
+              <div>
+                <img id="coneImg" src="./images/cone.jpg" alt="empty cone" />
+              </div>
+              <div>
+              Photo by <a href="https://unsplash.com/es/@picoftasty?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Mae Mu</a> 
+              on <a href="https://unsplash.com/images/food/ice-cream?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+              </div>
           </main>
       </Def>
     )
--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,13 @@ function home () {
       <Def>
           <main>
               <h1>HOME</h1>
+              <div>
+                <img id='saladImg' src="./images/saladbowl.jpg" alt="salad bowl" />
+              </div>
+              <div>
+                Photo by <a href="https://unsplash.com/@pwign?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Anh Nguyen</a> 
+                on <a href="https://unsplash.com/images/food?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+              </div>
               <a href="/places">
                 <button className='btn-primary'>Places Page</button>
               </a>
