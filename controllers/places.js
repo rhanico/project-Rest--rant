c7e6ed69f61a27bfc8f88a3f96cc72c1184dbd0a
FRANZ RHANICO <109740904+rhanico@users.noreply.github.com>
2023-05-04 03:26:25 -0700

    fixinf edit page

--- controllers/places.js
+++ controllers/places.js
@@ -39,20 +39,6 @@ router.get('/:id', (req, res) => {
   }
 })
 
-router.get('/:id/edit', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-      res.render('error404')
-  }
-  else if (!places[id]) {
-      res.render('error404')
-  }
-  else {
-    res.render('places/edit', { place: places[id], id })
-  }
-})
-
-
 
 router.delete('/:id', (req, res) => {
   let id = Number(req.params.id)
@@ -67,6 +53,20 @@ router.delete('/:id', (req, res) => {
     res.redirect('/places')
   }
 })
+
+router.get('/:id/edit', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+      res.render('error404')
+  }
+  else if (!places[id]) {
+      res.render('error404')
+  }
+  else {
+    res.render('places/edit', { place: places[id], id })
+  }
+})
+
 
 
 
--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -2,70 +2,64 @@ const React = require('react')
 const Def = require('../default.jsx')
 
 function edit_form (data) {
-    return(
+    return (
         <Def>
             <html>
                 <head>
-                    <title>EDIT PLACE</title>
+                    <title>title</title>
                     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" />
                     <link rel="stylesheet" href="/css/style.css" />
                 </head>
-                <body>
-                    <main>
-                        <form id="newForm" method="POST" action="/places">
-                            <div className='row'>
-                                <div className="form-group col-sm-6">
-                                    <label htmlFor="name">Place Name</label>
-                                    <input 
-                                        className="form-control" 
-                                        id="name" 
-                                        name="name" 
-                                        value={data.place.name}
-                                    required />
-                                </div>
-                                <div className="form-group col-sm-6">
-                                    <label htmlFor="pic">Place Picture</label>
-                                    <input 
-                                        className="form-control" 
-                                        id="name" 
-                                        name="name" 
-                                        value={data.place.pic}
-                                    required />
-                                </div>
-                            </div>
-                            <div className='row'>
-                                <div className="form-group col-sm-6">
-                                    <label htmlFor="city">City</label>
-                                    <input 
-                                        className="form-control" 
-                                        id="name" 
-                                        name="name" 
-                                        value={data.place.city}
-                                    required />
-                                </div>
-                                <div className="form-group col-sm-6">
-                                    <label htmlFor="state">State</label>
-                                    <input 
-                                        className="form-control" 
-                                        id="name" 
-                                        name="name" 
-                                        value={data.place.state}
-                                    required />
-                                </div>
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="cuisines">Cuisines</label>
-                                <input 
-                                    className="form-control" 
-                                    id="name" 
-                                    name="name" 
-                                    value={data.place.cuisines}
-                                required />
-                            </div>
-                            <input className="btn btn-primary" type="submit" value="Add Place" />
-                        </form>
-                    </main>
-                </body>
+                <main>
+                <form id="newForm" method="POST" action={`/places/${data.place.id}?_method=PUT`}>
+                    <div className="form-group">
+                        <label htmlFor="name">Place Name</label>
+                        <input 
+                            className="form-control" 
+                            id="name" 
+                            name="name" 
+                            value={`${data.place.name}`}
+                        required />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="pic">Place Picture</label>
+                        <input 
+                            className="form-control" 
+                            id="name" 
+                            name="name" 
+                            value={`${data.place.pic}`}
+                        required />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="city">City</label>
+                        <input 
+                            className="form-control" 
+                            id="name" 
+                            name="name" 
+                            value={`${data.place.city}`}
+                        required />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="state">State</label>
+                        <input 
+                            className="form-control" 
+                            id="name" 
+                            name="name" 
+                            value={`${data.place.state}`}
+                        required />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="cuisines">Cuisines</label>
+                        <input 
+                            className="form-control" 
+                            id="name" 
+                            name="name" 
+                            value={`${data.place.cuisines}`}
+                        required />
+                    </div>
+                    <input className="btn btn-primary" type="submit" value="Add Place" />
+                </form>
+                </main>
             </html>
         </Def>
     )
