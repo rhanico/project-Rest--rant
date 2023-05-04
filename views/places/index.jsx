7b7978f5d5cc0b1be7a0b9ce9006f560adc8e429
FRANZ RHANICO <109740904+rhanico@users.noreply.github.com>
2023-05-03 20:15:38 -0700

    show route connected

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -5,13 +5,17 @@ function index (data) {
     let placesFormatted = data.places.map((place) => {
       return (
         <div className='col-sm-6'>
-          <h2>{place.name}</h2>
+          <h2>
+            <a href= {`/places/${index}`} >
+              {place.name}
+              </a>
+          </h2>
           <p className='text-center'>
             {place.cuisines}
           </p>
           <img src={place.pic} alt={place.name}/>
           <p className='text-center'>
-            Located in {place.cuisines}, {place.state}
+            Located in {place.city}, {place.state}
           </p>
         </div>
       )
